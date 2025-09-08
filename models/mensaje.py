b6e3a8279e0a7a43db69c1a37876fe01ca5c4538
theizok <ramirezizok@gmail.com>
2025-09-08 11:22:31 -0500

    Primer commit

---
+++ models/mensaje.py
@@ -0,0 +1,9 @@
+from senasoft import db
+from datetime import datetime
+
+class Mensaje(db.Model):
+    mensaje_id = db.Column(db.Integer, primary_key=True)
+    conversacion_id = db.Column(db.Integer, db.ForeignKey("conversacion.id", nullable=False))
+    contenido = db.Column(db.Text, nullable=False)
+
+    fecha_envio = db.Column(db.DateTime, default=datetime.now)
